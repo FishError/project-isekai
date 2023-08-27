959e23a70664fbe6bccccd185a44bdf9a1fb8aed
Alex Hung <alexh4949@gmail.com>
2023-08-27 10:45:32 -0400

    slope check

--- Assets/Scripts/Entities/PhysicsEntities/PhysicsEntity.cs
+++ Assets/Scripts/Entities/PhysicsEntities/PhysicsEntity.cs
@@ -8,10 +8,10 @@ using UnityEngine.InputSystem.HID;
 public class PhysicsEntity : Entity
 {
     public float gravity { get;  protected set; } = 9.81f;
-
-    public Vector2 tempVelocity;
-    public List<Vector2> velocities;
     public bool grounded;
+
+    public List<Vector2> velocities;
+    private Vector2 tempVelocity;
 
     public Rigidbody2D rb { get; protected set; }
 
@@ -31,6 +31,7 @@ public class PhysicsEntity : Entity
         CheckGrounded();
         ApplyGravity();
         CalculateNetVelocity();
+        CheckSlope();
     }
 
     private void CalculateNetVelocity()
@@ -49,9 +50,10 @@ public class PhysicsEntity : Entity
         float distance = Mathf.Abs(rb.velocity.y) * Time.fixedDeltaTime;
         Bounds bounds = GetComponentInChildren<BoxCollider2D>().bounds;
         Vector3 raySpacing = new Vector3(bounds.size.x / 2, 0);
+        RaycastHit2D hit;
         for (int i = 0; i < 3; i++)
         {
-            RaycastHit2D hit = Physics2D.Raycast(bounds.min + raySpacing * i, Vector2.down, (distance < 0.05f) ? 0.05f : distance);
+            hit = Physics2D.Raycast(bounds.min + raySpacing * i, Vector2.down, (distance < 0.05f) ? 0.05f : distance);
             if (hit.collider != null)
             {
                 float angle = Vector2.Angle(Vector2.down, hit.normal);
@@ -62,6 +64,24 @@ public class PhysicsEntity : Entity
                 return;
             }
             grounded = false;
+        }
+    }
+
+    private void CheckSlope()
+    {
+        float distance = Mathf.Abs(rb.velocity.x) * Time.fixedDeltaTime;
+        Bounds bounds = GetComponentInChildren<BoxCollider2D>().bounds;
+        RaycastHit2D hit;
+        if (rb.velocity.x > 0)
+            hit = Physics2D.Raycast(bounds.min + new Vector3(bounds.size.x, 0), new Vector2(rb.velocity.x, 0), (distance < 0.05f) ? 0.05f : distance);
+        else
+            hit = Physics2D.Raycast(bounds.min, new Vector2(rb.velocity.x, 0), (distance < 0.05f) ? 0.05f : distance);
+
+        if (hit.collider != null)
+        {
+            float angle = Vector2.Angle(Vector2.down, hit.normal);
+            if (180 - angle >= 31)
+                rb.velocity = new Vector2(0, rb.velocity.y);
         }
     }
 
